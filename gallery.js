9e1b1e272e9e378f1d34994e84b01cc096c31bf5
Glitch (working-smallwebsite) <none>
2023-08-15 01:58:16 +0000

    🟣👄 Updated with Glitch

---
+++ gallery.js
@@ -0,0 +1,48 @@
+/* This music thing uses an API called 'Open Sheet' to get Google Sheets as JSON. Unlike the 'Inspiration' page that uses the 'papaparse' library to turn CSV into JSON, this API does it for us. 
+
+Learn more at https://github.com/benborgers/opensheet
+*/
+
+/*
+  The API's format is https://opensheet.elk.sh/spreadsheet_id/tab_name.
+  
+  
+  To get your spreadsheet ID, hit Share at the top-right and make sure ANYONE WITH THE LINK CAN VIEW. Then, copy the end of your URL in your address bar after docs.google.com/spreadsheets/d/...
+  
+  e.g.
+  https://docs.google.com/spreadsheets/d/1gLyh6gv41vl9H1fBmjFrGfKip9dKkvIu6o8sccTw2lY/edit#gid=1875797309
+  copy
+  "1gLyh6gv41vl9H1fBmjFrGfKip9dKkvIu6o8sccTw2lY" is your spreadsheet ID.
+  
+  We're using the 'Cloud' tab, so this is what we write...
+*/
+
+/* 
+
+OK so the plan of action here is to 
+
+*/
+
+var SPREADSHEET_ID_AND_TAB = "1v6YroQmKUuXuZL-qDdwkx_0pRwYGrJqhT_TBxFPKnNc/1";
+
+$(document).ready(function () {
+  $.getJSON("https://opensheet.elk.sh/" + SPREADSHEET_ID_AND_TAB, function (data) {
+    
+    console.log(data);
+    
+    data.reverse().forEach(function (row, index) {
+    /*  if (index === 0) return; //deleted this, since there is an index in my spreadsheet*/
+
+ let div = $(`<div class="item"> 
+        <img src="` + row.photo + `">
+        <h3>` + row.identity +`</h3>
+        <p>` +  row.text + `</p>
+        <p>` +  row.Timestamp + `</p>
+        <p>` +  row.intentions + `</p>
+        </div>`)
+      .appendTo("#content"); // # refers to div id
+    });
+  });
+});
+
+
---
+++ remember/love.js
@@ -0,0 +1,50 @@
+var SPREADSHEET_ID_AND_TAB = "1tCaj0sIz934xK_ZQjz4Jp-8W0mPrJYnQCClq8VtLIy4/1";
+
+$(document).ready(function () {
+  var contentItems = []; // Array to store the content items
+  var currentIndex = 0; // Current index of the displayed content item
+
+  // Retrieve the data and populate contentItems array
+  $.getJSON("https://opensheet.elk.sh/" + SPREADSHEET_ID_AND_TAB, function (data) {
+    contentItems = data;
+  }).done(function () {
+    // Event listener for the button that triggers the pop-up window
+    $(".popup-trigger button").click(function () {
+      displayContentItem(currentIndex);
+    });
+  });
+
+  // Display a content item in the pop-up window
+  function displayContentItem(index) {
+    var item = contentItems[index];
+    var content = item.text;
+
+    // Update the content of the pop-up window
+    $("#popup-content").text(content);
+
+    // Show the pop-up window
+    $("#popup").show();
+  }
+
+  // Navigation functions
+  function navigatePrev() {
+    currentIndex = (currentIndex - 1 + contentItems.length) % contentItems.length;
+    displayContentItem(currentIndex);
+  }
+
+  function navigateNext() {
+    currentIndex = (currentIndex + 1) % contentItems.length;
+    displayContentItem(currentIndex);
+  }
+
+  // Event handlers for navigation buttons
+  $("#popup-prev").click(navigatePrev);
+  $("#popup-next").click(navigateNext);
+  $("#popup-close").click(function () {
+    $("#popup").hide();
+  });
+});
+
+$(function() {
+  $("#popup").draggable();
+});
