0cf203da3ad51364cb639126b5260d9eb78296ed
Glitch (working-smallwebsite) <none>
2023-11-07 21:54:48 +0000

    🕓🎹 Updated with Glitch

---
+++ ily/index.js
@@ -0,0 +1,31 @@
+ var SPREADSHEET_ID_AND_TAB = "1p42Rd2JZLqaz5-Onc-0yvWWDfESvNf4rkcrQl7ReESc/1";
+
+$(document).ready(function () {
+  var contentItems = []; // Array to store the content items
+  var currentIndex = 0; // Current index of the displayed content item
+
+  // Retrieve the data and populate contentItems array
+  $.getJSON("https://opensheet.elk.sh/" + SPREADSHEET_ID_AND_TAB, function (data) {
+    contentItems = data;
+  }).done(function () {
+    var displayContainer = document.getElementById("display-container");
+    var refreshButton = document.getElementById("refresh-button");
+
+    // Function to display a random cell from a specific column
+    function displayRandomCell() {
+      if (contentItems.length > 0) {
+        var randomIndex = Math.floor(Math.random() * contentItems.length);
+        var randomCellValue = contentItems[randomIndex].alternate; // Replace 'yourColumnName' with the actual column name
+
+        // Display the cell value in your container (e.g., a <div>)
+        displayContainer.textContent = randomCellValue;
+      }
+    }
+ refreshButton.innerHTML = "💞";
+    // Event listener for the refresh button
+    refreshButton.addEventListener("click", displayRandomCell);
+
+    // Initial display
+    displayRandomCell();
+  });
+});
